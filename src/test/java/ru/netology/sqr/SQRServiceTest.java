ea4fe75bc7f3971030954e14ce9ceb02134f8dc8
Nasya Kabakova <kabakova.nas@gmail.com>
2023-02-26 22:10:40 +0300

    Add test

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.sqr;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 public class SQRServiceTest {
     @org.junit.jupiter.api.Test
     void shouldCalculateForCorrectValue() {
@@ -10,6 +11,22 @@ public class SQRServiceTest {
         int first = 200;
         int end = 300;
         long expected = 3;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(first, end);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForInCorrectValue() {
+        SQRService service = new SQRService();
+
+        // подготавливаем данные:
+        int first = 500;
+        int end = 100;
+        long expected = 0;
 
         // вызываем целевой метод:
         long actual = service.calculate(first, end);
